4cd1be54e80421a86b56328effb02016a8085af2
lxzanaa <zari.weone2010@gmail.com>
2024-07-30 21:00:07 +0300

    add

--- public/js/modal.js
+++ public/js/modal.js
@@ -27,3 +27,33 @@ questionAnswerOverlay.addEventListener('click', closeModalQues);
 questionAnswer.addEventListener('click', (event) => {
     event.stopPropagation();
 });
+
+const OrderModal = document.querySelector('#order-modal');
+const OrderModalOverlay = document.querySelector('#order-overlay');
+const OrderOpenModalButtons = document.querySelectorAll('[id^="order-button"]');
+
+function OrderOpenModal() {
+    OrderModalOverlay.classList.remove('hidden');
+    OrderModalOverlay.classList.add('opacity-100');
+    OrderModal.classList.remove('hidden', 'opacity-0', 'scale-90');
+    OrderModal.classList.add('opacity-100', 'scale-100');
+    document.body.style.overflow = 'hidden';
+}
+
+function OrderCloseModal() {
+    OrderModal.classList.remove('opacity-100', 'scale-100');
+    OrderModal.classList.add('opacity-0', 'scale-90' , 'hidden');
+    OrderModalOverlay.classList.remove('opacity-100');
+    OrderModalOverlay.classList.add('hidden');
+    document.body.style.overflow = '';
+}
+
+OrderOpenModalButtons.forEach(function (button) {
+    button.addEventListener('click', OrderOpenModal);
+});
+
+OrderModalOverlay.addEventListener('click', OrderCloseModal);
+
+OrderModal.addEventListener('click', (event) => {
+    event.stopPropagation();
+});
